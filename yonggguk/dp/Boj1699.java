413d21d64519f8849bfd1cbd644526b0ec7e5459
User <yonguk929@gmail.com>
2024-09-20 22:33:20 +0900

    add : Boj1699 (dp) *재풀이*

---
+++ yonggguk/dp/Boj1699.java
@@ -0,0 +1,24 @@
+package dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj1699 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[n + 1];
+        for(int i = 0; i <= n; i++){
+            dp[i] = i;
+        }
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j*j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+        System.out.println(dp[n]);
+    }
+}
